c200337d97f4e0214261f2f7b6ab6e278bf6dfc3
dwkns <dwkns@me.com>
2025-03-10 21:14:59 +0000

    Initial commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,18 @@
+import { execSync } from 'child_process'
 
-module.exports = (eleventyConfig) => {
-  return {
-    dir: {
-      input: "src",
-    },
-  };
+export default (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy('src/css/styles.css');
+  eleventyConfig.addWatchTarget("src/css/styles.css");
+  eleventyConfig.on("eleventy.after", ({ directories: { output: o } }) => {
+    execSync(
+      `npx @tailwindcss/cli -i ${o}css/styles.css -o ${o}css/styles.css`
+    );
+  });
+};
+
+export const config = {
+  dir: {
+    input: "src",
+    output: "dist"
+  },
 };
